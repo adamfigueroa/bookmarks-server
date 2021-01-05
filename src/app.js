56650758878347753e704f23df29dbe5ca897f9c
Adam Figueroa <admiralfiggins@gmail.com>
2021-01-05 12:07:52 -0800

    added 2 get requests

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const bookmarks = require("./bookmarks");
 
 const app = express();
 
@@ -13,8 +14,23 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+// Write a route handler for the endpoint GET /bookmarks
+// that returns a list of bookmarks
+
+app.get("/bookmarks", (req, res) => {
+  res.json(bookmarks);
+});
+
+// Write a route handler for the endpoint GET /bookmarks/:id
+// that returns a single bookmark with the given ID, return
+// 404 Not Found if the ID is not valid
+
+app.get("/bookmarks/:id", (req, res) => {
+  const { id } = req.params;
+  const requestedBookmark = bookmarks.find((bookmark) => bookmark.id === id);
+  if (requestedBookmark === undefined) {
+    return res.status(400).send(`There were no matches containing "${id}" `);
+  } else return res.json(requestedBookmark);
 });
 
 app.use(function errorHandler(error, req, res, next) {
---
+++ src/bookmarks.js
@@ -0,0 +1,32 @@
+const bookmarks = [
+  {
+    id: "test",
+    title: "Thinkful",
+    url: "https://www.thinkful.com",
+    description: "Think outside the classroom",
+    rating: 5,
+  },
+  {
+    id: "test2",
+    title: "Facebook",
+    url: "https://www.facebook.com",
+    description: "Think outside the classroom",
+    rating: 5,
+  },
+  {
+    id: "test3",
+    title: "Google",
+    url: "https://www.thinkful.com",
+    description: "Think outside the classroom",
+    rating: 5,
+  },
+  {
+    id: "test4",
+    title: "MDN",
+    url: "https://developer.mozilla.com",
+    description: "Think outside the classroom",
+    rating: 5,
+  },
+];
+
+module.exports = bookmarks
